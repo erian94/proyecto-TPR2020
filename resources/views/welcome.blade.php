f6b5f9494c20ea3698f32e5041ec7646cb876dad
erian94 <eriantercerosrivera@gmail.com>
2020-09-30 23:56:13 -0400

    welcome change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,8 +55,8 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Laravel has wonderful, thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we recommend reading all of the documentation from beginning to end.
-                                </div>
+                                    tec. de la prog. en red. Probando. Despues cambio lo demas    
+                                  </div>
                             </div>
                         </div>
 
